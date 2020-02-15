aadf19e6ac3d51e5290b905a20fab501779900bf
binayakranjan <39010182+binayakranjan@users.noreply.github.com>
2020-02-15 15:34:29 -0500

    Create nn-one-shot.py

---
+++ nn-one-shot.py
@@ -0,0 +1,21 @@
+def nearest_neighbour_correct(pairs,targets):
+    """returns 1 if nearest neighbour gets the correct answer for a one-shot task
+        given by (pairs, targets)"""
+    L2_distances = np.zeros_like(targets)
+    for i in range(len(targets)):
+        L2_distances[i] = np.sum(np.sqrt(pairs[0][i]**2 - pairs[1][i]**2))
+    if np.argmin(L2_distances) == np.argmax(targets):
+        return 1
+    return 0
+
+  
+  def test_nn_accuracy(N_ways,n_trials):
+    """Returns accuracy of NN approach """
+    print("Evaluating nearest neighbour on {} unique {} way one-shot learning tasks ...".format(n_trials,N_ways))
+    n_right = 0
+    
+    for i in range(n_trials):
+        pairs,targets = make_oneshot_task(N_ways,"val")
+        correct = nearest_neighbour_correct(pairs,targets)
+        n_right += correct
+    return 100.0 * n_right / n_trials
