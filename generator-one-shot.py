911f86fbe351168d9ce44b91b22733249ecb2728
binayakranjan <39010182+binayakranjan@users.noreply.github.com>
2020-02-15 15:31:47 -0500

    Create generator-one-shot.py

---
+++ generator-one-shot.py
@@ -0,0 +1,7 @@
+def generate(batch_size, s="train"):
+    """
+    a generator for batches, so model.fit_generator can be used.
+    """
+    while True:
+        pairs, targets = get_batch(batch_size,s)
+        yield (pairs, targets)
